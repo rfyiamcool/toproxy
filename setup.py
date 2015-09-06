e058e1af7f1678696022376d4b65fbd9012b6c27
芮峰云 <rfyiamcool@163.com>
2015-09-06 18:57:23 +0800

    add pypi more info

--- setup.py
+++ setup.py
@@ -4,14 +4,27 @@ from distutils.core import setup, Command
 import os
 import os.path
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name='toproxy',
     version='2.0',
     description='Simple Tornado Async HTTP Proxy',
+    long_description=open('README.md').read(),
+    keywords = ["tornado proxy","fengyun"],
     url='http://xiaorui.cc',
     author='ruifengyun',
     author_email='rfyiamcool@163.com',
-    long_description=open('README.md').read(),
     install_requires=['tornado'],
     packages=['toproxy'],
+    license = "MIT",
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+            ]
 )
